2891320b3bda1e446fdf5ca3e8e422c6b79ea82a
Stefan Mermans <stefanmermans99@gmail.com>
2023-09-20 22:47:51 +0200

    Simple threading example

--- src/http/server.rs
+++ src/http/server.rs
@@ -1,30 +1,31 @@
 use std::io::{Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
 use crate::http::parse::parse_http_stream;
-use crate::http::request::Request;
+use crate::match_request;
 
 const ADDRESS: &str = "127.0.0.1:7878";
 
-pub fn start_http_server<F>(on_request: &F)
-    where
-    F: Fn(&mut Request) -> Vec<u8>
+pub fn start_http_server()
 {
     let listener = TcpListener::bind(ADDRESS).expect("Failed to bind http server");
     println!("Server started!");
     println!("Listening on: http://{}", ADDRESS);
 
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream, on_request);
+
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
-fn handle_connection<F>(mut stream: TcpStream, on_request: &F)
-    where
-    F: Fn(&mut Request) -> Vec<u8>
+fn handle_connection(mut stream: TcpStream)
 {
     let mut request = parse_http_stream(&stream);
-    let response = on_request(&mut request);
+    let response = match_request(&mut request);
 
     stream.write_all(response.as_slice()).expect("Failed to send response!");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+use std::time::Duration;
 use crate::http::request::Request;
 use crate::http::response::file_response::FileResponse;
 use crate::http::response::Response;
@@ -6,17 +8,17 @@ use crate::http::server::start_http_server;
 mod http;
 
 fn main() {
-    start_http_server(&match_request);
+    start_http_server();
 }
 
-fn match_request(request: &mut Request) -> Vec<u8>
+pub fn match_request(request: &mut Request) -> Vec<u8>
 {
     println!("Request: {:#?}", request);
 
     match request.path.as_str() {
         "/" => FileResponse::ok("public/index.html".to_string()).to_response_data_bytes(),
         "/sleep" => {
-
+            thread::sleep(Duration::from_secs(5));
             FileResponse::ok("public/index.html".to_string()).to_response_data_bytes()
         },
         _ => FileResponse::ok("public/404.html".to_string()).to_response_data_bytes(),
